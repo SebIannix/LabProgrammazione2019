46b581e87edc91bfc81e6b2b38ac343f6b0c9b67
Marco Casazza <marco.casazza@unimi.it>
2019-12-11 13:04:29 +0100

    Aggiunti esercizi traccia Laboratorio 9

---
+++ Laboratorio_9/Strutture/esercizio_1/esempio.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Persona struct {
+	Nome    string
+	Cognome string
+}
+
+func main() {
+
+	var p1, p2, p5 Persona
+	// ... equivalente a:
+	//p1 = Persona{}
+	//p2 = Persona{}
+	//p5 = Persona{}
+
+	p1 = Persona{"Rick", "Sanchez"}
+
+	p2.Nome = "Mario"
+	p2.Cognome = "Rossi"
+
+	p3 := Persona{"Morty", "Smith"}
+	p4 := Persona{Cognome: "Bianchi", Nome: "Paolo"}
+
+	p6 := Persona{Cognome: "Rossi"}
+
+	fmt.Println("p1:", p1)
+	fmt.Println("p5:", p5)
+
+	nome := "Giuseppe"
+	cognome := "Verdi"
+
+	p5 = Persona{Cognome: cognome, Nome: nome}
+	// ... equivalente a:
+	// p5 = Persona{nome, cognome}
+
+	fmt.Printf("Nome: %-10s - Cognome: %-10s\n", p1.Nome, p1.Cognome)
+	fmt.Printf("%T\t%v\n", p3, p3)
+	fmt.Printf("%#v\n", p4)
+	fmt.Printf("%#v\n", p6)
+
+}
---
+++ Laboratorio_9/Strutture/esercizio_2/esempio.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Persona struct {
+	Nome    string
+	Cognome string
+}
+
+func main() {
+
+	anagrafica := []Persona{
+		{"Rick", "Sanchez"},
+		{"Morty", "Smith"},
+		{"Jerry", "Smith"},
+		{"Summer", "Smith"},
+		{"Beth", "Smith"}}
+
+	fmt.Printf("Tipo anagrafica %T\n", anagrafica)
+	fmt.Println()
+
+	for _, p := range anagrafica {
+		fmt.Println(p.Nome, p.Cognome)
+	}
+	fmt.Println()
+
+	CambiaPrimoCognome(anagrafica)
+
+	for _, p := range anagrafica {
+		fmt.Println(p.Nome, p.Cognome)
+	}
+}
+
+func CambiaPrimoCognome(a []Persona){
+	a[0].Cognome = "Martinez"
+}
